0d8523708f110ef4bb83406459c3702c195e175c
Emiliano <alohacocki@gmail.com>
2024-05-01 18:00:30 -0300

    Implemented Weapon Logic

--- P1_Parcial_1_Martel_Emiliano/Ejercicio1/Weapon.cpp
+++ P1_Parcial_1_Martel_Emiliano/Ejercicio1/Weapon.cpp
@@ -9,5 +9,16 @@ Weapon::Weapon(string name, WeaponType weaponType, float attack, float critRate,
 }
 
 float Weapon::getDamage(AttackType attackType, float critRateReduction, bool isCrit) {
+	int max = 100;
+	int chances = rand() % 100;
+	int damage = attack;
 
+	isCrit = false;
+
+	if (chances <= critRate - critRateReduction) {
+		attack += critDamage;
+		isCrit = true;
+	}
+
+	return attack;
 }
--- P1_Parcial_1_Martel_Emiliano/Ejercicio1/WeaponType.h
+++ P1_Parcial_1_Martel_Emiliano/Ejercicio1/WeaponType.h
@@ -1,5 +1,6 @@
 #pragma once
 enum WeaponType {
+	Grips,
 	Sword,
 	Lancer,
 	asd
